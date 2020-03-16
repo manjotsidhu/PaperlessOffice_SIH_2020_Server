2bc4834a85725a523876e671c54adc63d9ffd91b
Manjot Sidhu <manjot.techie@gmail.com>
2020-03-16 12:17:08 +0530

    Models: User: Add more fields

--- database/models.py
+++ database/models.py
@@ -4,8 +4,11 @@ from flask_bcrypt import generate_password_hash, check_password_hash
 
 class User(db.Document):
     email = db.StringField(required=True, unique=True)
+    first_name = db.StringField(required=True)
+    last_name = db.StringField(required=True)
     password = db.StringField(required=True, min_length=6)
-    role = db.StringField(required=True)
+    dob = db.DateTimeField()
+    role = db.StringField(default='user')
 
     def hash_password(self):
         self.password = generate_password_hash(self.password).decode('utf8')

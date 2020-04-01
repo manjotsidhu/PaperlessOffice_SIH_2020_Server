8814eb499df60c227718949680aaa0365ddf3373
Manjot Sidhu <manjot.techie@gmail.com>
2020-04-01 19:06:09 +0530

    enforce security on Form API

--- resources/form.py
+++ resources/form.py
@@ -1,4 +1,5 @@
 import bson
+from flask_jwt_extended import jwt_required
 
 from flask_restful import Resource
 from flask import Response, request, jsonify
@@ -7,6 +8,7 @@ from database.models import Form, Field
 
 
 class SaveFormApi(Resource):
+    @jwt_required
     def post(self):
         body = request.get_json()
         form = Form(**body).save()
@@ -15,5 +17,6 @@ class SaveFormApi(Resource):
 
 
 class GetFormApi(Resource):
+    @jwt_required
     def get(self, id):
         return Response(Form.objects.get(id=id).to_json(), mimetype="application/json", status=200)

43648c8431d9e2d854063124eedaa48f64eeadd4
Manjot Sidhu <manjot.techie@gmail.com>
2020-07-30 00:04:31 +0530

    Bringup Forms GET API.
    Signed-off-by: Manjot Sidhu <manjot.techie@gmail.com>

--- resources/form.py
+++ resources/form.py
@@ -1,9 +1,11 @@
 from flask_jwt_extended import jwt_required
 
 from flask_restful import Resource
-from flask import Response, request
+from flask import Response, request, send_from_directory
 
 from database.models import Form
+from resources.utils import UPLOAD_FOLDER, get_user_id
+from services.export2excel.export2excel import export_to_excel
 
 
 class FormApi(Resource):
@@ -12,6 +14,15 @@ class FormApi(Resource):
         body = request.get_json()
         form = Form(**body).save()
         return {'id': str(form.id)}, 200
+
+    @jwt_required
+    def get(self):
+        forms = Form.objects()
+
+        if 'excel' in request.args:
+            return send_from_directory(directory=UPLOAD_FOLDER, filename=export_to_excel(forms, get_user_id()))
+
+        return Response(forms.to_json(), mimetype="application/json", status=200)
 
 
 class GetFormApi(Resource):
